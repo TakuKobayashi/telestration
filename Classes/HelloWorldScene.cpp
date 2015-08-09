77db8bafcb05385b26445e74bcb117d7a89bbc01
小林拓 <keep_slimbody@yahoo.co.jp>
2015-08-09 11:51:06 +0900

    手書きができていたものを引っ張ってきた

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -47,42 +47,110 @@ bool HelloWorld::init()
     auto menu = Menu::create(closeItem, NULL);
     menu->setPosition(Vec2::ZERO);
     this->addChild(menu, 1);
-
+    
     /////////////////////////////
     // 3. add your codes below...
 
     // add a label shows "Hello World"
     // create and initialize a label
     
-    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
+    auto renderTexture = RenderTexture::create(visibleSize.width, visibleSize.height);
+    renderTexture->setPosition(Vec2(visibleSize.width/2, visibleSize.height/2));
+    this->addChild(renderTexture, 2);
+    //drawSegment(スタートポイント,エンドポイント,太さ,色);
     
-    // position the label on the center of the screen
-    label->setPosition(Vec2(origin.x + visibleSize.width/2,
-                            origin.y + visibleSize.height - label->getContentSize().height));
-
-    // add the label as a child to this layer
-    this->addChild(label, 1);
-
-    // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
-
-    // position the sprite on the center of the screen
-    sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
-
-    // add the sprite as a child to this layer
-    this->addChild(sprite, 0);
+    auto httpRequest = new HttpRequest();
     
+    httpRequest->setUrl("http://www.google.co.jp");
+    httpRequest->setRequestType(HttpRequest::Type::GET);
+    httpRequest->setResponseCallback([this](HttpClient* client, HttpResponse* response){
+        std::vector<char>* data = response->getResponseData();
+        std::string result(data->begin(), data->end());
+        //CCLOG(result.c_str());
+    });
+    
+    network::HttpClient::getInstance()->send(httpRequest);
+    
+    posList = std::vector<Vec2>();
+    
+    //イベントリスナーを作成
+    auto listener = EventListenerTouchOneByOne::create();
+
+    //タッチ開始
+    listener->onTouchBegan = [=](Touch* touch, Event* event){
+        prevPoint = touch->getLocation();
+        posList.push_back(prevPoint);
+        log("TouchBegan");
+        return true;
+    };
+    
+    //タッチ中
+    listener->onTouchMoved = [=](Touch* touch, Event* event){
+        renderTexture->begin();
+        auto drawNode = DrawNode::create();
+        drawNode->drawSegment(prevPoint, touch->getLocation(), 2.0f, Color4F(.5f, 1.0f, .7f, 1.0f));
+        drawNode->visit(); // RenderTextureにレンダリング
+        renderTexture->end();
+        prevPoint = touch->getLocation();
+        posList.push_back(prevPoint);
+
+        log("TouchMoved");
+    };
+    
+    //タッチ終了
+    listener->onTouchEnded = [=](Touch* touch, Event* event){
+        //CCLOG(HelloWorld::createJson().c_str());
+        //posList
+        /*
+        for (auto pos : posList) {
+          CCLOG("%f:%f", pos.x, pos.y);
+        }*/
+        posList.clear();
+        log("TouchEnded");
+    };
+    
+    //イベントリスナーを登録
+    this->getEventDispatcher()->addEventListenerWithSceneGraphPriority(listener, this);
+
     return true;
+}
+
+std::string HelloWorld::createJson() {
+    // JSON生成
+    Document document;
+    document.SetObject();
+    rapidjson::Document::AllocatorType& allocator = document.GetAllocator();
+    
+    // JSON配列生成
+    rapidjson::Value jsonArray(rapidjson::kArrayType);
+    // 要素生成
+    for (auto pos : posList) {
+        rapidjson::Value object(rapidjson::kObjectType);
+        object.AddMember<const float>("x",  pos.x, allocator);
+        object.AddMember<const float>("y", pos.y, allocator);
+        // 配列に追加
+        jsonArray.PushBack(object, allocator);
+    }
+    // JSON配列追加
+    //document.AddMember(ARRY_ROOT, jsonArray, allocator);
+    
+    // StringBufferへ書き出し
+    rapidjson::StringBuffer buffer;
+    rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+    jsonArray.Accept(writer);
+    
+    // JSON文字列取得
+    const char* result = buffer.GetString();
+    
+    // string生成
+    std::string str(result);
+    
+    return str;
 }
 
 
 void HelloWorld::menuCloseCallback(Ref* pSender)
 {
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_WP8) || (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
-	MessageBox("You pressed the close button. Windows Store Apps do not implement a close button.","Alert");
-    return;
-#endif
-
     Director::getInstance()->end();
 
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -3,8 +3,23 @@
 
 #include "cocos2d.h"
 
+#include "json/rapidjson.h"
+#include "json/document.h"
+#include "json/stringbuffer.h"
+#include "json/writer.h"
+
+#include "network/HttpClient.h"
+
+USING_NS_CC;
+using namespace rapidjson;
+using namespace cocos2d::network;
+
 class HelloWorld : public cocos2d::Layer
 {
+private:
+    cocos2d::Vec2 prevPoint;
+    std::vector<cocos2d::Vec2> posList;
+    std::string createJson();
 public:
     // there's no 'id' in cpp, so we recommend returning the class instance pointer
     static cocos2d::Scene* createScene();
